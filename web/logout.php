77cdc3080ddaba669e4e60005adbbef99d29d31e
Tomo <38649730+Tomopu@users.noreply.github.com>
2022-04-03 11:39:26 +0900

    Delete web directory

--- web/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();  // セッションの中身を削除
-session_destroy();    // セッションを破壊
-?>
-
-<p>ログアウトしました。</p>
-<a href="login.php">ログインへ</a>
-<a href="index.php">ホームへ</a>
